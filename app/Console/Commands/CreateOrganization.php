165c820dc86d13f4b87332c510ae168f2c8b1dd4
Sophea Phy <sophea@web-essentials.co>
2022-03-02 17:05:32 +0700

    [TASK] Set system limit default value
    refs HIG-8

--- app/Console/Commands/CreateOrganization.php
+++ app/Console/Commands/CreateOrganization.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\Organization;
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class CreateOrganization extends Command
 {
@@ -39,13 +40,17 @@ class CreateOrganization extends Command
      */
     public function handle()
     {
+        $system_limit = json_decode(Storage::get('system_limit/settings.json'));
         $user = User::where('type', User::ADMIN_GROUP_GLOBAL_ADMIN)->first();
+
         if ($user) {
             Organization::create([
                 'name' => 'Humanity Inclusion',
                 'type' => Organization::HI_TYPE,
                 'admin_email' => $user->email,
                 'sub_domain_name' => 'hi',
+                'max_number_of_therapist' => $system_limit->therapist_content_limit,
+                'max_ongoing_treatment_plan' => $system_limit->number_of_ongoing_treatment_per_therapist,
             ]);
         }
 
