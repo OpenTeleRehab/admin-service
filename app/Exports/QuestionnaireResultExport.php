bac9832139f92fd276278005a7e6f4ca2354538e
Lina Sun <lina@web-essentials.co>
2025-04-08 15:55:36 +0700

    [TASK] Add more patient info in report
    refs TRA-1018

--- app/Exports/QuestionnaireResultExport.php
+++ app/Exports/QuestionnaireResultExport.php
@@ -5,6 +5,8 @@ namespace App\Exports;
 use App\Helpers\TranslationHelper;
 use App\Models\Forwarder;
 use App\Models\Questionnaire;
+use App\Models\Country;
+use App\Models\InternationalClassificationDisease;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
@@ -74,22 +76,52 @@ class QuestionnaireResultExport
             $sheet->mergeCells('C1:C2');
             $sheet->mergeCells('D1:D2');
             $sheet->mergeCells('E1:E2');
+            $sheet->mergeCells('F1:F2');
+            $sheet->mergeCells('G1:G2');
+            $sheet->mergeCells('H1:H2');
+            $sheet->mergeCells('I1:I2');
+            $sheet->mergeCells('J1:J2');
+            $sheet->mergeCells('K1:K2');
+            $sheet->mergeCells('L1:L2');
+            $sheet->mergeCells('M1:M2');
 
             $sheet->setCellValue('A1', $translations['report.questionnaire_result.patient_id']);
-            $sheet->setCellValue('B1', $translations['report.questionnaire_result.diagnostic']);
-            $sheet->setCellValue('C1', $translations['common.start_date']);
-            $sheet->setCellValue('D1', $translations['common.end_date']);
-            $sheet->setCellValue('E1', $translations['common.submitted_date']);
+            $sheet->setCellValue('B1', $translations['common.country']);
+            $sheet->setCellValue('C1', $translations['common.gender']);
+            $sheet->setCellValue('D1', $translations['common.date_of_birth']);
+            $sheet->setCellValue('E1', $translations['common.age']);
+            $sheet->setCellValue('F1', $translations['common.status']);
+            $sheet->setCellValue('G1', $translations['common.location']);
+            $sheet->setCellValue('H1', $translations['report.questionnaire_result.icd_classification']);
+            $sheet->setCellValue('I1', $translations['report.questionnaire_result.diagnostic']);
+            $sheet->setCellValue('J1', $translations['common.start_date']);
+            $sheet->setCellValue('K1', $translations['common.end_date']);
+            $sheet->setCellValue('L1', $translations['common.submitted_date']);
+            $sheet->setCellValue('M1', $translations['report.questionnaire_result.questionnaire_name']);
             $sheet->getColumnDimension('A')->setWidth(20);
             $sheet->getColumnDimension('B')->setWidth(20);
             $sheet->getColumnDimension('C')->setWidth(20);
             $sheet->getColumnDimension('D')->setWidth(20);
             $sheet->getColumnDimension('E')->setWidth(20);
+            $sheet->getColumnDimension('F')->setWidth(20);
+            $sheet->getColumnDimension('G')->setWidth(20);
+            $sheet->getColumnDimension('H')->setWidth(20);
+            $sheet->getColumnDimension('I')->setWidth(20);
+            $sheet->getColumnDimension('J')->setWidth(20);
+            $sheet->getColumnDimension('K')->setWidth(20);
+            $sheet->getColumnDimension('L')->setWidth(20);
+            $sheet->getColumnDimension('M')->setWidth(20);
 
-            $colIndex = 6;
+            $colIndex = 14;
             foreach ($questions as $question) {
-                // Each question spans 3 columns
-                $endColIndex = $colIndex + 2;
+                $endColIndex = $colIndex;
+                // Each question spans 3 columns for open number and 2 for multiple and checkbox
+                if ($question->type === Question::QUESTION_TYPE_OPEN_NUMBER) {
+                    $endColIndex = $colIndex + 2;
+                } else if ($question->type !== Question::QUESTION_TYPE_OPEN_TEXT) {
+                    $endColIndex = $colIndex + 1;
+                }
+                
                 // Convert numeric column index to Excel column letters
                 $startCol = Coordinate::stringFromColumnIndex($colIndex);
                 $endCol = Coordinate::stringFromColumnIndex($endColIndex);
@@ -99,14 +131,23 @@ class QuestionnaireResultExport
 
                 // Answer row title
                 $sheet->setCellValue($startCol . '2', $translations['report.questionnaire_result.answer']);
-                $sheet->setCellValue(Coordinate::stringFromColumnIndex($colIndex + 1) . '2', $translations['report.questionnaire_result.value']);
-                $sheet->setCellValue(Coordinate::stringFromColumnIndex($colIndex + 2) . '2', $translations['report.questionnaire_result.threshold']);
+                if ($question->type !== Question::QUESTION_TYPE_OPEN_TEXT) {
+                    $sheet->setCellValue(Coordinate::stringFromColumnIndex($colIndex + 1) . '2', $translations['report.questionnaire_result.value']);
+                    $sheet->getColumnDimension(Coordinate::stringFromColumnIndex($colIndex + 1))->setWidth(20);
+                }
+                
+                if ($question->type === Question::QUESTION_TYPE_OPEN_NUMBER) {
+                    $sheet->setCellValue(Coordinate::stringFromColumnIndex($colIndex + 2) . '2', $translations['report.questionnaire_result.threshold']);
+                    $sheet->getColumnDimension(Coordinate::stringFromColumnIndex($colIndex + 2))->setWidth(20);
+                }
                 $sheet->getColumnDimension($startCol)->setWidth(20);
-                $sheet->getColumnDimension(Coordinate::stringFromColumnIndex($colIndex + 1))->setWidth(20);
-                $sheet->getColumnDimension(Coordinate::stringFromColumnIndex($colIndex + 2))->setWidth(20);
 
-                // Move to the next question (3 columns forward)
-                $colIndex += 3;
+                // Move to the next question
+                if ($question->type === Question::QUESTION_TYPE_OPEN_NUMBER) {
+                    $colIndex += 3;
+                } else if ($question->type !== Question::QUESTION_TYPE_OPEN_TEXT) {
+                    $colIndex += 2;
+                }
             }
 
             if (isset($endCol)) {
@@ -127,14 +168,35 @@ class QuestionnaireResultExport
                 $startDate = Carbon::createFromFormat('d/m/Y', $treatmentPlan['start_date'])->format('Y-m-d');
                 $endDate = Carbon::createFromFormat('d/m/Y', $treatmentPlan['end_date'])->format('Y-m-d');
                 $submittedDate = Carbon::createFromFormat('d/m/Y', $activity['submitted_date'])->format('Y-m-d');
+                $country = Country::find($patient['country_id']);
+                $age = Carbon::parse($patient['date_of_birth'])->age;
+                $dob = Carbon::parse($patient['date_of_birth'])->toDateString();
+                $status = $patient['enabled'] === 1 ? $translations['common.active'] : $translations['common.inactive'];
+                $location = $translations['common.' . $patient['location']];
+                $gender = $translations['common.' . $patient['gender']];
+                $disease = InternationalClassificationDisease::find($treatmentPlan['disease_id']);
+                $diseasName = $disease?->getTranslation('name', $payload['lang'] ?? 'en');
 
                 $sheet->setCellValue('A' . $row, $patient['identity'] ?? '');
                 $sheet->setCellValue('B' . $row, $treatmentPlan['name'] ?? '');
                 $sheet->setCellValue('C' . $row, $startDate);
                 $sheet->setCellValue('D' . $row, $endDate);
                 $sheet->setCellValue('E' . $row, $submittedDate);
+                $sheet->setCellValue('A' . $row, $patient['identity'] ?? '');
+                $sheet->setCellValue('B' . $row, $country?->name ?? '');
+                $sheet->setCellValue('C' . $row, $gender);
+                $sheet->setCellValue('D' . $row, $dob);
+                $sheet->setCellValue('E' . $row, $age);
+                $sheet->setCellValue('F' . $row, $status);
+                $sheet->setCellValue('G' . $row, $location);
+                $sheet->setCellValue('H' . $row, $diseasName);
+                $sheet->setCellValue('I' . $row, $treatmentPlan['name'] ?? '');
+                $sheet->setCellValue('J' . $row, $startDate);
+                $sheet->setCellValue('K' . $row, $endDate);
+                $sheet->setCellValue('L' . $row, $submittedDate);
+                $sheet->setCellValue('M' . $row, $questionnaire['title']);
 
-                $colIndex = 6;
+                $colIndex = 14;
                 foreach ($questions as $question) {
                     $patientAnswer = null;
                     foreach ($questionnaireAnswers as $questionnaireAnswer) {
@@ -154,12 +216,10 @@ class QuestionnaireResultExport
                             $foundAnswers = $question->answers->filter(fn($questionAnswer) => in_array($questionAnswer->id, $answer))->all();
                             $answerDescriptions = array_column($foundAnswers, 'description');
                             $values = array_column($foundAnswers, 'value');
-                            $thresholds = array_column($foundAnswers, 'threshold');
                         } else if ($question->type === Question::QUESTION_TYPE_MULTIPLE) {
                             $foundAnswer = $question->answers->first(fn($questionAnswer) => $questionAnswer->id === $answer);
                             $answerDescriptions[] = $foundAnswer->description;
                             $values[] = $foundAnswer->value ?? '';
-                            $thresholds[] = $foundAnswer->threshold ?? '';
                         } else if ($question->type === Question::QUESTION_TYPE_OPEN_NUMBER) {
                             $foundAnswer = $question->answers->first(fn($questionAnswer) => $questionAnswer->question_id === $question->id);
                             $answerDescriptions[] = $answer;
@@ -190,8 +250,12 @@ class QuestionnaireResultExport
                         }
                     }
 
-                    // Move to the next set of columns (3 columns forward)
-                    $colIndex += 3;
+                    // Move to the next set of columns
+                    if ($question->type === Question::QUESTION_TYPE_OPEN_NUMBER) {
+                        $colIndex += 3;
+                    } else if ($question->type !== Question::QUESTION_TYPE_OPEN_TEXT) {
+                        $colIndex += 2;
+                    }
                 }
                 $sheet->getRowDimension($row)->setRowHeight(20);
                 $row++;
