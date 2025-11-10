11b708e6393fdc62a0a8dafe89c766af80221b4e
Chivy Lim <chivy@web-essentials.co>
2025-11-10 13:58:57 +0700

    [TASK] Add description field to assistive tech list
    refs TRA-1097

--- app/Http/Resources/AssistiveTechnologyListResource.php
+++ app/Http/Resources/AssistiveTechnologyListResource.php
@@ -18,6 +18,7 @@ class AssistiveTechnologyListResource extends JsonResource
             'id' => $this->id,
             'code' => $this->code,
             'name' => $this->name,
+            'description' => $this->description,
             'isUsed' => $this->isUsed(),
             'deleted_at' => $this->deleted_at,
         ];
