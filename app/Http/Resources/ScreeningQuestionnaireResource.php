1604288137fa7648ace7f01526136dee54a3b77e
Rathana Hak <rathana@web-essentials.co>
2025-12-19 13:16:24 +0700

    [TASK] Get list history screening questionnaire
    refs TRA-1116

--- app/Http/Resources/ScreeningQuestionnaireResource.php
+++ app/Http/Resources/ScreeningQuestionnaireResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\ScreeningQuestionnaireAnswer;
 use App\Models\ScreeningQuestionnaireQuestion;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,15 +16,20 @@ class ScreeningQuestionnaireResource extends JsonResource
      */
     public function toArray($request)
     {
+        $userId = $request->get('user_id');
         return [
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'sections' => ScreeningQuestionnaireSectionResource::collection($this->sections),
             'total_question' => ScreeningQuestionnaireQuestion::where('questionnaire_id', $this->id)->count(),
+            'total_interview_history' => ScreeningQuestionnaireAnswer::where('questionnaire_id', $this->id)
+            ->where('user_id', $userId)
+            ->count(),
             'published_date' => $this->published_date,
             'status' => $this->status,
             'auto_translated' => $this->auto_translated,
         ];
     }
 }
+
